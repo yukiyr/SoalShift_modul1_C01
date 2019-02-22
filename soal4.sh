f8e960e0c69def7f2b20733f9da8af8cb7c358e0
yukiyr <37041284+yukiyr@users.noreply.github.com>
2019-02-22 10:43:32 +0000

    Update soal4.sh

--- soal4.sh
+++ soal4.sh
@@ -7,6 +7,10 @@ alpha=({a..z})
 encrypt=( "${alpha[@]:(-(26-$jam))}" )
 encrypt+=( "${alpha[@]:0:$jam}" )
 
-awk '{print}' /var/log/syslog | tr "${alpha[*]}" "${encrypt[*]}"  > "$today".log
+alpha1=({A..Z})
+encrypt1=( "${alpha1[@]:(-(26-$jam))}" )
+encrypt1+=( "${alpha1[@]:0:$jam}" )
+
+awk '{print}' /var/log/syslog | tr "${alpha[*]}" "${encrypt[*]}"| tr "${alpha1[*]}" "${encrypt1[*]}"  > "$today".log
 
 #crontab = */60 * * * * /bin/bash /home/test/soal4.sh
