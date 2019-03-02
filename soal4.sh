37bdfc6148eef6bc177bb0bfeff407cc534769ef
yukiyr <37041284+yukiyr@users.noreply.github.com>
2019-03-02 11:24:04 +0700

    Update soal4.sh

--- soal4.sh
+++ soal4.sh
@@ -1,3 +1,4 @@
+- Enkripsi
 #!/bin/bash
 
 today="$( date +"%H:%M %d-%m-%Y" )"
@@ -14,3 +15,16 @@ encrypt1+=( "${alpha1[@]:0:$jam}" )
 awk '{print}' /var/log/syslog | tr "${alpha[*]}" "${encrypt[*]}"| tr "${alpha1[*]}" "${encrypt1[*]}"  > "$today".log
 
 #crontab = */60 * * * * /bin/bash /home/test/soal4.sh
+
+- Dekripsi
+#!/bin/bash
+
+hurufkecil=(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+hurufbesar=(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
+
+echo “Enter the filename :”
+read hour
+jam=${hour:0:2}
+
+bawah=${hurufkecil[$jam]}
+atas=${hurufbesar[$jam]}
